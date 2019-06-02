450ab06bc98f9c8cbabec68049d2cfc21f24a028
Nguyen Nhu Phuong <phuongbau12@gmail.com>
2019-06-02 20:44:20 +0700

    bt

---
+++ SS7/PT ung dung.js
@@ -0,0 +1,26 @@
+function Apple(weigth){
+    this.weight = 10;
+    this.setAppleweight = function (weight) {
+        return  weight;
+    }
+}
+function Human(name, weight, gender) {
+    this.name = name;
+    this.weight = weight;
+    this.gender = true;
+    this.setSayhuman = function (human) {
+        return human;
+    };
+    this.setGender = function () {
+        return gender;
+
+    };
+    this.checkApple = function () {
+        if( apple => 1){
+            return true;
+        }
+        return false;
+    }
+
+
+}
